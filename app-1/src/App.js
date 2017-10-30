ce2d1a7d4bb5aaea25259ec18f1c0725aa2d2dc0
deltmd <remidy@gmail.com>
2017-10-30 17:19:06 -0600

    Completed 1,2,and 3.

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    }
+  }
+  handleChange(str){
+    this.setState({
+      message: str
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" onChange={(e) => this.handleChange(e.target.value)}/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foods: [
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese"
+      ],
+      filter: ''
+    }
+  }
+  handleUpdate(val){
+    this.setState({
+      filter: val
+    })
+  }
+
   render() {
+    let foodsToDisplay = this.state.foods.map((e, i) => {
+      if (e.indexOf(this.state.filter)>-1) {
+        return (<h2 key={i}>{e}</h2>)
+      }
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" name="" id="" onChange={e=>this.handleUpdate(e.target.value)}/>
+        {foodsToDisplay}
       </div>
     );
   }
